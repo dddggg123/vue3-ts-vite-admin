e9e8ef28546d34165b9aac0a0f00729d38f771bd
王鑫 <18827029354@163.com>
2022-04-20 10:56:42 +0800

    更新提交代码

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import'./styles/index.scss'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
 createApp(App)
     .use(router)
+    .use(ElementPlus)
     .mount('#app')
