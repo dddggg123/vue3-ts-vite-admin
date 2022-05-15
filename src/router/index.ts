226985d49fe4d7a0ea09f602119bb718d22228f9
Gridfas <809418027@qq.com>
2022-05-15 11:57:39 +0800

    更新代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -108,14 +108,6 @@ export const DynamicRoutes = [
                     icon: 'grid'
                 }
             }, {
-                path: 'contact',
-                name: 'contact',
-                component: () => import('@/pages/contact/contact.vue'),
-                meta: {
-                    name: '联系我们',
-                    icon: 'phone'
-                }
-            }, {
                 path: 'map',
                 name: 'map',
                 component: () => import('@/pages/map/map.vue'),
@@ -162,6 +154,14 @@ export const DynamicRoutes = [
                 meta: {
                     name: '其他模块',
                     icon: 'link'
+                }
+            }, {
+                path: 'contact',
+                name: 'contact',
+                component: () => import('@/pages/contact/contact.vue'),
+                meta: {
+                    name: '联系作者',
+                    icon: 'phone'
                 }
             }
         ]
