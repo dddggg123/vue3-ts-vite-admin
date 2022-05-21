051af5ad4ea5a585db515d34d518b4970b11c791
Gridfas <809418027@qq.com>
2022-05-21 18:30:11 +0800

    update code and push

--- src/pages/screen/screen.vue
+++ src/pages/screen/screen.vue
@@ -1,10 +1,331 @@
 <template>
-  <div class="other-container">这里是大屏界面</div>
+  <div class="other-container">
+    <div ref="line" class="screen-item"></div>
+    <div ref="pie" class="screen-item"></div>
+    <div ref="heatmap" class="screen-item"></div>
+    <div ref="radar" class="screen-item"></div>
+    <div ref="funnel" class="screen-item"></div>
+    <div ref="gauge" class="screen-item"></div>
+  </div>
 </template>
 
-<script lang="ts">
-export default {
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+//  按需引入 echarts
+import * as echarts from "echarts";
+const line = ref() // 使用ref创建虚拟DOM引用
+const pie = ref() // 使用ref创建虚拟DOM引用
+const heatmap = ref()
+const radar = ref()
+const funnel = ref()
+const gauge = ref()
 
+onMounted(() => {
+  initLineChart();
+  initPieChart();
+  initHeatmapChart();
+  initRadarChart();
+  initFunnelChart();
+  initGaugeChart();
+}
+)
+function initGaugeChart() {
+  // 基于准备好的dom，初始化echarts实例
+  let myChart = echarts.init(gauge.value);
+  // 指定图表的配置项和数据
+  let option = {
+    tooltip: {
+    formatter: '{a} <br/>{b} : {c}%'
+  },
+  series: [
+    {
+      name: 'Pressure',
+      type: 'gauge',
+      progress: {
+        show: true
+      },
+      detail: {
+        valueAnimation: true,
+        formatter: '{value}'
+      },
+      data: [
+        {
+          value: 50,
+          name: 'SCORE'
+        }
+      ]
+    }
+  ]
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+}
+function initFunnelChart() {
+  // 基于准备好的dom，初始化echarts实例
+  let myChart = echarts.init(funnel.value);
+  // 指定图表的配置项和数据
+  let option = {
+    title: {
+      text: 'Funnel'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c}%'
+    },
+    toolbox: {
+      feature: {
+        dataView: { readOnly: false },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    legend: {
+      data: ['Show', 'Click', 'Visit', 'Inquiry', 'Order']
+    },
+    series: [
+      {
+        name: 'Expected',
+        type: 'funnel',
+        left: '10%',
+        width: '80%',
+        label: {
+          formatter: '{b}Expected'
+        },
+        labelLine: {
+          show: false
+        },
+        itemStyle: {
+          opacity: 0.7
+        },
+        emphasis: {
+          label: {
+            position: 'inside',
+            formatter: '{b}Expected: {c}%'
+          }
+        },
+        data: [
+          { value: 60, name: 'Visit' },
+          { value: 40, name: 'Inquiry' },
+          { value: 20, name: 'Order' },
+          { value: 80, name: 'Click' },
+          { value: 100, name: 'Show' }
+        ]
+      },
+      {
+        name: 'Actual',
+        type: 'funnel',
+        left: '10%',
+        width: '80%',
+        maxSize: '80%',
+        label: {
+          position: 'inside',
+          formatter: '{c}%',
+          color: '#fff'
+        },
+        itemStyle: {
+          opacity: 0.5,
+          borderColor: '#fff',
+          borderWidth: 2
+        },
+        emphasis: {
+          label: {
+            position: 'inside',
+            formatter: '{b}Actual: {c}%'
+          }
+        },
+        data: [
+          { value: 30, name: 'Visit' },
+          { value: 10, name: 'Inquiry' },
+          { value: 5, name: 'Order' },
+          { value: 50, name: 'Click' },
+          { value: 80, name: 'Show' }
+        ],
+        // Ensure outer shape will not be over inner shape when hover.
+        z: 100
+      }
+    ]
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+}
+function initRadarChart() {
+  // 基于准备好的dom，初始化echarts实例
+  let myChart = echarts.init(radar.value);
+  // 指定图表的配置项和数据
+  let option = {
+    title: {
+      text: 'Radar Chart'
+    },
+    legend: {
+      data: ['Allocated Budget', 'Actual Spending']
+    },
+    radar: {
+      // shape: 'circle',
+      indicator: [
+        { name: 'Sales', max: 6500 },
+        { name: 'Administration', max: 16000 },
+        { name: 'Information Technology', max: 30000 },
+        { name: 'Customer Support', max: 38000 },
+        { name: 'Development', max: 52000 },
+        { name: 'Marketing', max: 25000 }
+      ]
+    },
+    series: [
+      {
+        name: 'Budget vs spending',
+        type: 'radar',
+        data: [
+          {
+            value: [4200, 3000, 20000, 35000, 50000, 18000],
+            name: 'Allocated Budget'
+          },
+          {
+            value: [5000, 14000, 28000, 26000, 42000, 21000],
+            name: 'Actual Spending'
+          }
+        ]
+      }
+    ]
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+}
+function initLineChart() {
+  // 基于准备好的dom，初始化echarts实例
+  let myChart = echarts.init(line.value);
+  // 指定图表的配置项和数据
+  let option = {
+    title: {
+      text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    legend: {
+      data: ['销量']
+    },
+    xAxis: {
+      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+}
+function initPieChart() {
+  // 基于准备好的dom，初始化echarts实例
+  let myChart = echarts.init(pie.value);
+  // 指定图表的配置项和数据
+  let option = {
+    title: {
+      text: 'Referer of a Website',
+      subtext: 'Fake Data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+}
+function initHeatmapChart() {
+  // 基于准备好的dom，初始化echarts实例
+  let myChart = echarts.init(heatmap.value);
+  const hours = [
+    '12a', '1a', '2a', '3a', '4a', '5a', '6a',
+    '7a', '8a', '9a', '10a', '11a',
+    '12p', '1p', '2p', '3p', '4p', '5p',
+    '6p', '7p', '8p', '9p', '10p', '11p'
+  ];
+  // prettier-ignore
+  const days = [
+    'Saturday', 'Friday', 'Thursday',
+    'Wednesday', 'Tuesday', 'Monday', 'Sunday'
+  ];
+  // prettier-ignore
+  const data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 6, 0], [0, 7, 0], [0, 8, 0], [0, 9, 0], [0, 10, 0], [0, 11, 2], [0, 12, 4], [0, 13, 1], [0, 14, 1], [0, 15, 3], [0, 16, 4], [0, 17, 6], [0, 18, 4], [0, 19, 4], [0, 20, 3], [0, 21, 3], [0, 22, 2], [0, 23, 5], [1, 0, 7], [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0], [1, 5, 0], [1, 6, 0], [1, 7, 0], [1, 8, 0], [1, 9, 0], [1, 10, 5], [1, 11, 2], [1, 12, 2], [1, 13, 6], [1, 14, 9], [1, 15, 11], [1, 16, 6], [1, 17, 7], [1, 18, 8], [1, 19, 12], [1, 20, 5], [1, 21, 5], [1, 22, 7], [1, 23, 2], [2, 0, 1], [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 5, 0], [2, 6, 0], [2, 7, 0], [2, 8, 0], [2, 9, 0], [2, 10, 3], [2, 11, 2], [2, 12, 1], [2, 13, 9], [2, 14, 8], [2, 15, 10], [2, 16, 6], [2, 17, 5], [2, 18, 5], [2, 19, 5], [2, 20, 7], [2, 21, 4], [2, 22, 2], [2, 23, 4], [3, 0, 7], [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 1], [3, 9, 0], [3, 10, 5], [3, 11, 4], [3, 12, 7], [3, 13, 14], [3, 14, 13], [3, 15, 12], [3, 16, 9], [3, 17, 5], [3, 18, 5], [3, 19, 10], [3, 20, 6], [3, 21, 4], [3, 22, 4], [3, 23, 1], [4, 0, 1], [4, 1, 3], [4, 2, 0], [4, 3, 0], [4, 4, 0], [4, 5, 1], [4, 6, 0], [4, 7, 0], [4, 8, 0], [4, 9, 2], [4, 10, 4], [4, 11, 4], [4, 12, 2], [4, 13, 4], [4, 14, 4], [4, 15, 14], [4, 16, 12], [4, 17, 1], [4, 18, 8], [4, 19, 5], [4, 20, 3], [4, 21, 7], [4, 22, 3], [4, 23, 0], [5, 0, 2], [5, 1, 1], [5, 2, 0], [5, 3, 3], [5, 4, 0], [5, 5, 0], [5, 6, 0], [5, 7, 0], [5, 8, 2], [5, 9, 0], [5, 10, 4], [5, 11, 1], [5, 12, 5], [5, 13, 10], [5, 14, 5], [5, 15, 7], [5, 16, 11], [5, 17, 6], [5, 18, 0], [5, 19, 5], [5, 20, 3], [5, 21, 4], [5, 22, 2], [5, 23, 0], [6, 0, 1], [6, 1, 0], [6, 2, 0], [6, 3, 0], [6, 4, 0], [6, 5, 0], [6, 6, 0], [6, 7, 0], [6, 8, 0], [6, 9, 0], [6, 10, 1], [6, 11, 0], [6, 12, 2], [6, 13, 1], [6, 14, 3], [6, 15, 4], [6, 16, 0], [6, 17, 0], [6, 18, 0], [6, 19, 0], [6, 20, 1], [6, 21, 2], [6, 22, 2], [6, 23, 6]].map(function (item) {
+    return [item[1], item[0], item[2] || '-'];
+  });
+  let option = {
+    tooltip: {
+      position: 'top'
+    },
+    grid: {
+      height: '50%',
+      top: '10%'
+    },
+    xAxis: {
+      type: 'category',
+      data: hours,
+      splitArea: {
+        show: true
+      }
+    },
+    yAxis: {
+      type: 'category',
+      data: days,
+      splitArea: {
+        show: true
+      }
+    },
+    visualMap: {
+      min: 0,
+      max: 10,
+      calculable: true,
+      orient: 'horizontal',
+      left: 'center',
+      bottom: '15%'
+    },
+    series: [
+      {
+        name: 'Punch Card',
+        type: 'heatmap',
+        data: data,
+        label: {
+          show: true
+        },
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
 }
 </script>
 
@@ -12,5 +333,12 @@ export default {
 .other-container {
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+
+  .screen-item {
+    width: calc(100% / 3);
+    height: 50%;
+  }
 }
 </style>
