4dd40ddc981392b23de560dc3751600e4d43d432
王鑫 <18827029354@163.com>
2022-05-23 09:57:53 +0800

    update

--- src/pages/micro/micro-app-one.vue
+++ src/pages/micro/micro-app-one.vue
@@ -4,7 +4,8 @@
     <div class="main-content">
       <h1>主应用操作界面</h1>
       <!-- <el-button class="btn" @click="mainBtnHandler" type="primary">主应用控制子应用跳转</el-button> -->
-      <el-button class="btn" @click="mainBtnHandlerWithParam" type="primary">主应用控制子应用跳转并传参: hello</el-button>
+      <el-button class="btn" @click="mainBtnHandlerWithParam1" type="primary">主应用控制子应用跳转并传参: Hello 采用localStorage</el-button>
+      <el-button class="btn" @click="mainBtnHandlerWithParam2" type="primary">主应用控制子应用跳转并传参: World! 采用sessionStorage</el-button>
     </div>
   </div>
 </template>
@@ -20,21 +21,30 @@ function mainBtnHandler () {
   // window.history.pushState({}, '', '/micro/app-one#/about')
   router.push('/micro/app-one#/about');
 }
-function mainBtnHandlerWithParam () {
+function mainBtnHandlerWithParam1 () {
   // window.history.pushState({
   //   msg: 'hello'
   // }, '', '/micro/app-one#/about')
   // router.push({path: '/micro/app-one#/about', query: {msg: 'hello'}});
-  localStorage.setItem('msg', 'hello');
+  localStorage.setItem('msg', 'Hello');
   router.push('/micro/app-one#/about');
+}
+function mainBtnHandlerWithParam2 () {
+  // window.history.pushState({
+  //   msg: 'hello'
+  // }, '', '/micro/app-one#/about')
+  // router.push({path: '/micro/app-one#/about', query: {msg: 'hello'}});
+  sessionStorage.setItem('desc', 'World!');
+  router.push('/micro/app-one#/about');
+  
 }
 </script>
 
 <style lang="scss">
 .micro-one-container {
-  width: 100%;
   padding: 0 5%;
   display: flex;
+  padding-top: 40px;
 
   #micro-content {
     width: 500px;
@@ -48,15 +58,14 @@ function mainBtnHandlerWithParam () {
   }
 
   .main-content {
-    width: 500px;
-    height: 800px;
+    width: 480px;
+    height: 600px;
     background-color: #a4a4a4;
     margin-left: 20px;
-    display: flex;
-    flex-direction: column;
+    padding-left: 20px;
     
     .btn {
-      width: 250px;
+      padding: 0 10px;
       height: 40px;
       border-radius: 10px;
       margin-top: 20px;
