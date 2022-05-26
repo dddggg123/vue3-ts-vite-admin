4d88083829260e3f049a03981d4a715185330110
Gridfas <809418027@qq.com>
2022-05-26 22:09:27 +0800

    update code and push

--- src/pages/screen/screen.vue
+++ src/pages/screen/screen.vue
@@ -34,26 +34,98 @@ function initGaugeChart() {
   let myChart = echarts.init(gauge.value);
   // 指定图表的配置项和数据
   let option = {
-    tooltip: {
-      formatter: '{a} <br/>{b} : {c}%'
+    title: {
+      text: 'Area'
     },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985'
+        }
+      }
+    },
+    legend: {
+      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
     series: [
       {
-        name: 'Pressure',
-        type: 'gauge',
-        progress: {
-          show: true
+        name: 'Email',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
         },
-        detail: {
-          valueAnimation: true,
-          formatter: '{value}'
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: 'Union Ads',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
         },
-        data: [
-          {
-            value: 50,
-            name: 'SCORE'
-          }
-        ]
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+        name: 'Video Ads',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [150, 232, 201, 154, 190, 330, 410]
+      },
+      {
+        name: 'Direct',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [320, 332, 301, 334, 390, 330, 320]
+      },
+      {
+        name: 'Search Engine',
+        type: 'line',
+        stack: 'Total',
+        label: {
+          show: true,
+          position: 'top'
+        },
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [820, 932, 901, 934, 1290, 1330, 1320]
       }
     ]
   };
@@ -65,83 +137,184 @@ function initFunnelChart() {
   let myChart = echarts.init(funnel.value);
   // 指定图表的配置项和数据
   let option = {
+    color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
     title: {
-      text: 'Funnel'
+      text: 'Gradient'
     },
     tooltip: {
-      trigger: 'item',
-      formatter: '{a} <br/>{b} : {c}%'
-    },
-    toolbox: {
-      feature: {
-        dataView: { readOnly: false },
-        restore: {},
-        saveAsImage: {}
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985'
+        }
       }
     },
     legend: {
-      data: ['Show', 'Click', 'Visit', 'Inquiry', 'Order']
+      data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']
     },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
     series: [
       {
-        name: 'Expected',
-        type: 'funnel',
-        left: '10%',
-        width: '80%',
-        label: {
-          formatter: '{b}Expected'
+        name: 'Line 1',
+        type: 'line',
+        stack: 'Total',
+        smooth: true,
+        lineStyle: {
+          width: 0
         },
-        labelLine: {
-          show: false
-        },
-        itemStyle: {
-          opacity: 0.7
+        showSymbol: false,
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgb(128, 255, 165)'
+            },
+            {
+              offset: 1,
+              color: 'rgb(1, 191, 236)'
+            }
+          ])
         },
         emphasis: {
-          label: {
-            position: 'inside',
-            formatter: '{b}Expected: {c}%'
-          }
+          focus: 'series'
         },
-        data: [
-          { value: 60, name: 'Visit' },
-          { value: 40, name: 'Inquiry' },
-          { value: 20, name: 'Order' },
-          { value: 80, name: 'Click' },
-          { value: 100, name: 'Show' }
-        ]
+        data: [140, 232, 101, 264, 90, 340, 250]
       },
       {
-        name: 'Actual',
-        type: 'funnel',
-        left: '10%',
-        width: '80%',
-        maxSize: '80%',
-        label: {
-          position: 'inside',
-          formatter: '{c}%',
-          color: '#fff'
+        name: 'Line 2',
+        type: 'line',
+        stack: 'Total',
+        smooth: true,
+        lineStyle: {
+          width: 0
         },
-        itemStyle: {
-          opacity: 0.5,
-          borderColor: '#fff',
-          borderWidth: 2
+        showSymbol: false,
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgb(0, 221, 255)'
+            },
+            {
+              offset: 1,
+              color: 'rgb(77, 119, 255)'
+            }
+          ])
         },
         emphasis: {
-          label: {
-            position: 'inside',
-            formatter: '{b}Actual: {c}%'
-          }
+          focus: 'series'
         },
-        data: [
-          { value: 30, name: 'Visit' },
-          { value: 10, name: 'Inquiry' },
-          { value: 5, name: 'Order' },
-          { value: 50, name: 'Click' },
-          { value: 80, name: 'Show' }
-        ],
-        // Ensure outer shape will not be over inner shape when hover.
-        z: 100
+        data: [120, 282, 111, 234, 220, 340, 310]
+      },
+      {
+        name: 'Line 3',
+        type: 'line',
+        stack: 'Total',
+        smooth: true,
+        lineStyle: {
+          width: 0
+        },
+        showSymbol: false,
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgb(55, 162, 255)'
+            },
+            {
+              offset: 1,
+              color: 'rgb(116, 21, 219)'
+            }
+          ])
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [320, 132, 201, 334, 190, 130, 220]
+      },
+      {
+        name: 'Line 4',
+        type: 'line',
+        stack: 'Total',
+        smooth: true,
+        lineStyle: {
+          width: 0
+        },
+        showSymbol: false,
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgb(255, 0, 135)'
+            },
+            {
+              offset: 1,
+              color: 'rgb(135, 0, 157)'
+            }
+          ])
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [220, 402, 231, 134, 190, 230, 120]
+      },
+      {
+        name: 'Line 5',
+        type: 'line',
+        stack: 'Total',
+        smooth: true,
+        lineStyle: {
+          width: 0
+        },
+        showSymbol: false,
+        label: {
+          show: true,
+          position: 'top'
+        },
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgb(255, 191, 0)'
+            },
+            {
+              offset: 1,
+              color: 'rgb(224, 62, 76)'
+            }
+          ])
+        },
+        emphasis: {
+          focus: 'series'
+        },
+        data: [220, 302, 181, 234, 210, 290, 150]
       }
     ]
   };
@@ -153,35 +326,122 @@ function initRadarChart() {
   let myChart = echarts.init(radar.value);
   // 指定图表的配置项和数据
   let option = {
+    color: ['#67F9D8', '#FFE434', '#56A3F1', '#FF917C'],
     title: {
-      text: 'Radar Chart'
+      text: 'Radar'
     },
-    legend: {
-      data: ['Allocated Budget', 'Actual Spending']
-    },
-    radar: {
-      // shape: 'circle',
-      indicator: [
-        { name: 'Sales', max: 6500 },
-        { name: 'Administration', max: 16000 },
-        { name: 'Information Technology', max: 30000 },
-        { name: 'Customer Support', max: 38000 },
-        { name: 'Development', max: 52000 },
-        { name: 'Marketing', max: 25000 }
-      ]
-    },
+    legend: {},
+    radar: [
+      {
+        indicator: [
+          { text: 'Indicator1' },
+          { text: 'Indicator2' },
+          { text: 'Indicator3' },
+          { text: 'Indicator4' },
+          { text: 'Indicator5' }
+        ],
+        center: ['25%', '50%'],
+        radius: 120,
+        startAngle: 90,
+        splitNumber: 4,
+        shape: 'circle',
+        axisName: {
+          formatter: '【{value}】',
+          color: '#428BD4'
+        },
+        splitArea: {
+          areaStyle: {
+            color: ['#77EADF', '#26C3BE', '#64AFE9', '#428BD4'],
+            shadowColor: 'rgba(0, 0, 0, 0.2)',
+            shadowBlur: 10
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            color: 'rgba(211, 253, 250, 0.8)'
+          }
+        },
+        splitLine: {
+          lineStyle: {
+            color: 'rgba(211, 253, 250, 0.8)'
+          }
+        }
+      },
+      {
+        indicator: [
+          { text: 'Indicator1', max: 150 },
+          { text: 'Indicator2', max: 150 },
+          { text: 'Indicator3', max: 150 },
+          { text: 'Indicator4', max: 120 },
+          { text: 'Indicator5', max: 108 },
+          { text: 'Indicator6', max: 72 }
+        ],
+        center: ['75%', '50%'],
+        radius: 120,
+        axisName: {
+          color: '#fff',
+          backgroundColor: '#666',
+          borderRadius: 3,
+          padding: [3, 5]
+        }
+      }
+    ],
     series: [
       {
-        name: 'Budget vs spending',
         type: 'radar',
+        emphasis: {
+          lineStyle: {
+            width: 4
+          }
+        },
         data: [
           {
-            value: [4200, 3000, 20000, 35000, 50000, 18000],
-            name: 'Allocated Budget'
+            value: [100, 8, 0.4, -80, 2000],
+            name: 'Data A'
           },
           {
-            value: [5000, 14000, 28000, 26000, 42000, 21000],
-            name: 'Actual Spending'
+            value: [60, 5, 0.3, -100, 1500],
+            name: 'Data B',
+            areaStyle: {
+              color: 'rgba(255, 228, 52, 0.6)'
+            }
+          }
+        ]
+      },
+      {
+        type: 'radar',
+        radarIndex: 1,
+        data: [
+          {
+            value: [120, 118, 130, 100, 99, 70],
+            name: 'Data C',
+            symbol: 'rect',
+            symbolSize: 12,
+            lineStyle: {
+              type: 'dashed'
+            },
+            label: {
+              show: true,
+              formatter: function (params: any) {
+                return params.value;
+              }
+            }
+          },
+          {
+            value: [100, 93, 50, 90, 70, 60],
+            name: 'Data D',
+            areaStyle: {
+              color: new echarts.graphic.RadialGradient(0.1, 0.6, 1, [
+                {
+                  color: 'rgba(255, 145, 124, 0.1)',
+                  offset: 0
+                },
+                {
+                  color: 'rgba(255, 145, 124, 0.9)',
+                  offset: 1
+                }
+              ])
+            }
           }
         ]
       }
@@ -195,24 +455,48 @@ function initLineChart() {
   let myChart = echarts.init(line.value);
   // 指定图表的配置项和数据
   let option = {
-    title: {
-      text: 'ECharts 入门示例'
+    angleAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     },
-    tooltip: {},
-    legend: {
-      data: ['销量']
-    },
-    xAxis: {
-      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    },
-    yAxis: {},
+    radiusAxis: {},
+    polar: {},
     series: [
       {
-        name: '销量',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [1, 2, 3, 4, 3, 5, 1],
+        coordinateSystem: 'polar',
+        name: 'A',
+        stack: 'a',
+        emphasis: {
+          focus: 'series'
+        }
+      },
+      {
+        type: 'bar',
+        data: [2, 4, 6, 1, 3, 2, 1],
+        coordinateSystem: 'polar',
+        name: 'B',
+        stack: 'a',
+        emphasis: {
+          focus: 'series'
+        }
+      },
+      {
+        type: 'bar',
+        data: [1, 2, 3, 4, 1, 2, 5],
+        coordinateSystem: 'polar',
+        name: 'C',
+        stack: 'a',
+        emphasis: {
+          focus: 'series'
+        }
       }
-    ]
+    ],
+    legend: {
+      show: true,
+      data: ['A', 'B', 'C']
+    }
   };
   // 使用刚指定的配置项和数据显示图表。
   myChart.setOption(option);
@@ -262,64 +546,44 @@ function initPieChart() {
 function initHeatmapChart() {
   // 基于准备好的dom，初始化echarts实例
   let myChart = echarts.init(heatmap.value);
-  const hours = [
-    '12a', '1a', '2a', '3a', '4a', '5a', '6a',
-    '7a', '8a', '9a', '10a', '11a',
-    '12p', '1p', '2p', '3p', '4p', '5p',
-    '6p', '7p', '8p', '9p', '10p', '11p'
-  ];
-  // prettier-ignore
-  const days = [
-    'Saturday', 'Friday', 'Thursday',
-    'Wednesday', 'Tuesday', 'Monday', 'Sunday'
-  ];
-  // prettier-ignore
-  const data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 6, 0], [0, 7, 0], [0, 8, 0], [0, 9, 0], [0, 10, 0], [0, 11, 2], [0, 12, 4], [0, 13, 1], [0, 14, 1], [0, 15, 3], [0, 16, 4], [0, 17, 6], [0, 18, 4], [0, 19, 4], [0, 20, 3], [0, 21, 3], [0, 22, 2], [0, 23, 5], [1, 0, 7], [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0], [1, 5, 0], [1, 6, 0], [1, 7, 0], [1, 8, 0], [1, 9, 0], [1, 10, 5], [1, 11, 2], [1, 12, 2], [1, 13, 6], [1, 14, 9], [1, 15, 11], [1, 16, 6], [1, 17, 7], [1, 18, 8], [1, 19, 12], [1, 20, 5], [1, 21, 5], [1, 22, 7], [1, 23, 2], [2, 0, 1], [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 5, 0], [2, 6, 0], [2, 7, 0], [2, 8, 0], [2, 9, 0], [2, 10, 3], [2, 11, 2], [2, 12, 1], [2, 13, 9], [2, 14, 8], [2, 15, 10], [2, 16, 6], [2, 17, 5], [2, 18, 5], [2, 19, 5], [2, 20, 7], [2, 21, 4], [2, 22, 2], [2, 23, 4], [3, 0, 7], [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 1], [3, 9, 0], [3, 10, 5], [3, 11, 4], [3, 12, 7], [3, 13, 14], [3, 14, 13], [3, 15, 12], [3, 16, 9], [3, 17, 5], [3, 18, 5], [3, 19, 10], [3, 20, 6], [3, 21, 4], [3, 22, 4], [3, 23, 1], [4, 0, 1], [4, 1, 3], [4, 2, 0], [4, 3, 0], [4, 4, 0], [4, 5, 1], [4, 6, 0], [4, 7, 0], [4, 8, 0], [4, 9, 2], [4, 10, 4], [4, 11, 4], [4, 12, 2], [4, 13, 4], [4, 14, 4], [4, 15, 14], [4, 16, 12], [4, 17, 1], [4, 18, 8], [4, 19, 5], [4, 20, 3], [4, 21, 7], [4, 22, 3], [4, 23, 0], [5, 0, 2], [5, 1, 1], [5, 2, 0], [5, 3, 3], [5, 4, 0], [5, 5, 0], [5, 6, 0], [5, 7, 0], [5, 8, 2], [5, 9, 0], [5, 10, 4], [5, 11, 1], [5, 12, 5], [5, 13, 10], [5, 14, 5], [5, 15, 7], [5, 16, 11], [5, 17, 6], [5, 18, 0], [5, 19, 5], [5, 20, 3], [5, 21, 4], [5, 22, 2], [5, 23, 0], [6, 0, 1], [6, 1, 0], [6, 2, 0], [6, 3, 0], [6, 4, 0], [6, 5, 0], [6, 6, 0], [6, 7, 0], [6, 8, 0], [6, 9, 0], [6, 10, 1], [6, 11, 0], [6, 12, 2], [6, 13, 1], [6, 14, 3], [6, 15, 4], [6, 16, 0], [6, 17, 0], [6, 18, 0], [6, 19, 0], [6, 20, 1], [6, 21, 2], [6, 22, 2], [6, 23, 6]].map(function (item) {
-    return [item[1], item[0], item[2] || '-'];
-  });
   let option = {
-    tooltip: {
-      position: 'top'
+    dataset: {
+      source: [
+        ['score', 'amount', 'product'],
+        [89.3, 58212, 'Matcha Latte'],
+        [57.1, 78254, 'Milk Tea'],
+        [74.4, 41032, 'Cheese Cocoa'],
+        [50.1, 12755, 'Cheese Brownie'],
+        [89.7, 20145, 'Matcha Cocoa'],
+        [68.1, 79146, 'Tea'],
+        [19.6, 91852, 'Orange Juice'],
+        [10.6, 101852, 'Lemon Juice'],
+        [32.7, 20112, 'Walnut Brownie']
+      ]
     },
-    grid: {
-      height: '50%',
-      top: '10%'
-    },
-    xAxis: {
-      type: 'category',
-      data: hours,
-      splitArea: {
-        show: true
-      }
-    },
-    yAxis: {
-      type: 'category',
-      data: days,
-      splitArea: {
-        show: true
-      }
-    },
+    grid: { containLabel: true },
+    xAxis: { name: 'amount' },
+    yAxis: { type: 'category' },
     visualMap: {
-      min: 0,
-      max: 10,
-      calculable: true,
       orient: 'horizontal',
       left: 'center',
-      bottom: '15%'
+      min: 10,
+      max: 100,
+      text: ['High Score', 'Low Score'],
+      // Map the score column to color
+      dimension: 0,
+      inRange: {
+        color: ['#65B581', '#FFCE34', '#FD665F']
+      }
     },
     series: [
       {
-        name: 'Punch Card',
-        type: 'heatmap',
-        data: data,
-        label: {
-          show: true
-        },
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
+        type: 'bar',
+        encode: {
+          // Map the "amount" column to X axis.
+          x: 'amount',
+          // Map the "product" column to Y axis
+          y: 'product'
         }
       }
     ]
