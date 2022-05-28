e912baa42d9d467f0eb9ae403dc6960752cb6566
王鑫 <18827029354@163.com>
2022-05-28 09:17:11 +0800

    修改首页描述

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -7,7 +7,7 @@
         <p class="reamrk-desc">2、本项目前端用来实践和学习Vue3、Vite、qiankun等技术栈。</p>
         <p class="reamrk-desc">3、本项目的CI/CD采用Jenkins结合Gitee。</p>
         <p class="reamrk-desc">4、本项目的服务端采用Windows Server 2012、宝塔面板结合Nginx。</p>
-        <p class="reamrk-desc">5、本项目的下载地址：</p>
+        <p class="reamrk-desc">5、本项目的下载地址：<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin">主应用</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-micro1">微前端应用1</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-micro2">微前端应用2</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-iframe">iframe项目</a></p>
       </div>
     </div>
   </div>
