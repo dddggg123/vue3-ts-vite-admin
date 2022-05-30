b5d84717105900aaa59acf20b52d93c6361938b6
Gridfas <809418027@qq.com>
2022-05-30 21:49:27 +0800

    同步代码

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,13 +2,15 @@
   <div class="home-container">
     <div class="remark-section">
       <div class="remark-content">
-        <p class="remark-title">使用说明</p>
+        <p class="remark-title">项目说明</p>
         <p class="reamrk-desc">1、本项目分为1个主项目、2个微前端项目和1个iframe项目，共4个项目。</p>
         <p class="reamrk-desc">2、本项目前端用来实践和学习Vue3、Vite、qiankun等技术栈。</p>
         <p class="reamrk-desc">3、本项目的CI/CD采用Jenkins结合Gitee。</p>
         <p class="reamrk-desc">4、本项目的服务端采用Windows Server 2012、宝塔面板结合Nginx。</p>
-        <p class="reamrk-desc">5、本项目的下载地址：<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin">主应用</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-micro1">微前端应用1</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-micro2">微前端应用2</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-iframe">iframe项目</a></p>
-      </div>
+        <p class="reamrk-desc">5、本项目Gitee下载地址：<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin">主应用</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-micro1">微前端应用1</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-micro2">微前端应用2</a>、<a target="_blank" href="https://gitee.com/xiaoxiang_reincarnation/vue3-ts-vite-admin-iframe">iframe项目</a></p>
+        <p class="reamrk-desc">6、本项目Github下载地址：<a target="_blank" href="https://github.com/dddggg123/vue3-ts-vite-admin">主应用</a>、<a target="_blank" href="https://github.com/dddggg123/vue3-ts-vite-admin-micro1">微前端应用1</a>、<a target="_blank" href="https://github.com/dddggg123/vue3-ts-vite-admin-micro2">微前端应用2</a>、<a target="_blank" href="https://github.com/dddggg123/vue3-ts-vite-admin-iframe">iframe项目</a></p>
+        <p class="reamrk-desc">7、如果你觉得本项目不错或者对自己工作有帮助，欢迎点个star。</p>
+     </div>
     </div>
   </div>
 </template>
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -63,7 +63,7 @@
         </el-tab-pane>
       </el-tabs>
       <div class="footer-section">
-        <span class="version">{{ $t('版本号') }}：1.0.22052802</span>
+        <span class="version">{{ $t('版本号') }}：1.0.22052901</span>
       </div>
     </div>
   </div>
